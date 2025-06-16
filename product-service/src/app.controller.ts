f0706fa99a85340b8f90f6c794f77a6a0f5d9bff
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-06-16 11:39:27 +0500

    Setup debugging for all services. Configure nodemon. Configure concurrently for running multiple services at once with some different colors

--- product-service/src/app.controller.ts
+++ product-service/src/app.controller.ts
@@ -5,6 +5,10 @@ import { AppService } from "./app.service";
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get("/")
+  home() {
+    return { message: "Product service home" };
+  }
   @Get("/ping")
   ping() {
     return { message: "Product service is alive" };
