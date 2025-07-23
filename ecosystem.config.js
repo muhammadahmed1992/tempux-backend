32aeace11131abff87b5c4f893ac4bcd379a9b6c
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-07-24 04:57:42 +0500

    using inject placeholders

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,3 @@
-// ecosystem.config.ts
-
 module.exports = {
   apps: [
     {
@@ -17,9 +15,9 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: "production",
-        AUTH_SERVICE_BASE_URL: process.env.AUTH_SERVICE_BASE_URL,
-        PRODUCT_SERVICE_BASE_URL: process.env.PRODUCT_SERVICE_BASE_URL,
-        PORT: process.env.GATEWAY_PORT,
+        AUTH_SERVICE_BASE_URL: "INJECT_AUTH_SERVICE_BASE_URL",
+        PRODUCT_SERVICE_BASE_URL: "INJECT_PRODUCT_SERVICE_BASE_URL",
+        PORT: "INJECT_GATEWAY_PORT",
       },
     },
     {
@@ -33,27 +31,28 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: "production",
-        JWT_SECRET: process.env.JWT_SECRET,
-        OTP_EXPIRY_DURATION: process.env.OTP_EXPIRY_DURATION,
-        MAIL_HOST: process.env.MAIL_HOST,
-        MAIL_PORT: process.env.MAIL_PORT,
-        MAIL_USERNAME: process.env.MAIL_USERNAME,
-        MAIL_PASSWORD: process.env.MAIL_PASSWORD,
-        MAIL_FROM: process.env.MAIL_FROM,
-        FRONTEND_URL: process.env.FRONTEND_URL,
+        JWT_SECRET: "INJECT_JWT_SECRET",
+        OTP_EXPIRY_DURATION: "INJECT_OTP_EXPIRY_DURATION",
+        MAIL_HOST: "INJECT_MAIL_HOST",
+        MAIL_PORT: "INJECT_MAIL_PORT",
+        MAIL_USERNAME: "INJECT_MAIL_USERNAME",
+        MAIL_PASSWORD: "INJECT_MAIL_PASSWORD",
+        MAIL_FROM: "INJECT_MAIL_FROM",
+        FRONTEND_URL: "INJECT_FRONTEND_URL",
 
-        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-        GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+        GOOGLE_CLIENT_ID: "INJECT_GOOGLE_CLIENT_ID",
+        GOOGLE_CLIENT_SECRET: "INJECT_GOOGLE_CLIENT_SECRET",
+        GOOGLE_CALLBACK_URL: "INJECT_GOOGLE_CALLBACK_URL",
 
-        FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
-        FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
-        FACEBOOK_CALLBACK_URL: process.env.FACEBOOK_CALLBACK_URL,
+        FACEBOOK_APP_ID: "INJECT_FACEBOOK_APP_ID",
+        FACEBOOK_APP_SECRET: "INJECT_FACEBOOK_APP_SECRET",
+        FACEBOOK_CALLBACK_URL: "INJECT_FACEBOOK_CALLBACK_URL",
 
-        EMAIL_ENCRYPTION_KEY: process.env.EMAIL_ENCRYPTION_KEY,
-        SALT_ROUND: process.env.SALT_ROUND,
-        PORT: process.env.AUTH_PORT,
-        DATABASE_URL_DEV_USERS: process.env.DATABASE_URL_DEV_USERS,
+        EMAIL_ENCRYPTION_KEY: "INJECT_EMAIL_ENCRYPTION_KEY",
+
+        SALT_ROUND: "INJECT_SALT_ROUND",
+        PORT: "INJECT_AUTH_PORT",
+        DATABASE_URL_DEV_USERS: "INJECT_DATABASE_URL_DEV_USERS",
       },
     },
     {
@@ -67,10 +66,10 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: "production",
-        JWT_SECRET: process.env.JWT_SECRET,
-        USER_SERVICE_URL: process.env.USER_SERVICE_URL,
-        PORT: process.env.PRODUCT_PORT,
-        DATABASE_URL_DEV_PRODUCT: process.env.DATABASE_URL_DEV_PRODUCT,
+        JWT_SECRET: "INJECT_JWT_SECRET",
+        USER_SERVICE_URL: "INJECT_USER_SERVICE_URL",
+        PORT: "INJECT_PRODUCT_PORT",
+        DATABASE_URL_DEV_PRODUCT: "INJECT_DATABASE_URL_DEV_PRODUCT",
       },
     },
   ],
