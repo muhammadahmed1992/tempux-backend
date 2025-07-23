6648023b4b419d7128bc3e9b94c93c61af8ebdd4
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-07-23 19:41:11 +0500

    Fix api-gateway proxy middleware issue

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ServiceResolver } from "@Config/service.resolver";
+import { ProxyMiddleware } from "./middleware/proxy-middleware";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ServiceResolver],
+  providers: [AppService, ServiceResolver, ProxyMiddleware],
 })
 export class AppModule {}
