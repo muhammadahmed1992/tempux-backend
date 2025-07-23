488a04008c401aca5a41da0c76fd779255f7a3e8
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-07-23 17:09:51 +0500

    Update ecosystem and workflow

--- ecosystem.config.ts
+++ ecosystem.config.ts
@@ -4,26 +4,61 @@ export default {
   apps: [
     {
       name: "api-gateway",
-      script: "./api-gateway/dist/main.js",
+      cwd: "./api-gateway",
+      script: "./dist/main.js",
+      autorestart: true,
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "development",
         PORT: 3000,
       },
     },
     {
       name: "auth-service",
-      script: "./auth-service/dist/main.js",
+      cwd: "./auth-service",
+      script: "./dist/main.js",
+      autorestart: true,
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "development",
         PORT: 3001,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        JWT_SECRET: "JWT_SECRET",
+        OTP_EXPIRY_DURATION: "OTP_EXPIRY_DURATION",
+        MAIL_HOST: "MAIL_HOST",
+        MAIL_PORT: "MAIL_PORT",
+        MAIL_USERNAME: "MAIL_USERNAME",
+        MAIL_PASSWORD: "MAIL_PASSWORD",
+        MAIL_FROM: "MAIL_FROM",
+        FRONTEND_URL: "FRONTEND_URL",
+
+        GOOGLE_CLIENT_ID: "GOOGLE_CLIENT_ID",
+        GOOGLE_CLIENT_SECRET: "GOOGLE_CLIENT_SECRET",
+        GOOGLE_CALLBACK_URL: "GOOGLE_CALLBACK_URL",
+
+        FACEBOOK_APP_ID: "FACEBOOK_URL",
+        FACEBOOK_APP_SECRET: "FACEBOOK_APP_SECRET",
+        FACEBOOK_CALLBACK_URL: "FACEBOOK_CALLBACK_URL",
+
+        EMAIL_ENCRYPTION_KEY: "EMAIL_ENCRYPTION_URL",
+
+        SALT_ROUND: "SALT_ROUND",
+        PORT: "AUTH_PORT",
       },
     },
     {
       name: "product-service",
-      script: "./product-service/dist/main.js",
+      cwd: "./product-service",
+      script: "./dist/main.js",
+      autorestart: true,
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "development",
         PORT: 3003,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        JWT_SECRET: "JWT_SECRET",
+        USER_SERVICE_URL: "USER_SERVICE_URL",
       },
     },
   ],
