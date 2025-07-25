b36e5f892d773d79d76ac28955d00b7adb8e1445
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-07-25 20:23:26 +0500

    update config module in api-gateway

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,28 +1,30 @@
-import { Module } from "@nestjs/common";
-import { UserController } from "@Controllers/user.controller";
-import { UserService } from "@Services/user.service";
-import { UserRepository } from "@Repository/users.repository";
-import { PrismaClient } from "@prisma/client";
-import { PrismaService } from "@Services/prisma.service";
-import { AppController } from "@Controllers/app.controller";
-import { JwtModule } from "@nestjs/jwt";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { GoogleStrategy } from "./social-login/google-strategy";
-import { FacebookStrategy } from "./social-login/facebook-strategy";
-import { EmailModule } from "@Module/email.module";
-import { EncryptionHelper } from "@Helper/encryption.helper";
+import { Module } from '@nestjs/common';
+import { UserController } from '@Controllers/user.controller';
+import { UserService } from '@Services/user.service';
+import { UserRepository } from '@Repository/users.repository';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from '@Services/prisma.service';
+import { AppController } from '@Controllers/app.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GoogleStrategy } from './social-login/google-strategy';
+import { FacebookStrategy } from './social-login/facebook-strategy';
+import { EmailModule } from '@Module/email.module';
+import { EncryptionHelper } from '@Helper/encryption.helper';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get("JWT_SECRET") || "yourSecretKeyHere",
-        signOptions: { expiresIn: "180d" },
+        secret: configService.get('JWT_SECRET') || 'yourSecretKeyHere',
+        signOptions: { expiresIn: '180d' },
       }),
       inject: [ConfigService],
     }),
-    ConfigModule,
     EmailModule,
   ],
   controllers: [AppController, UserController],
