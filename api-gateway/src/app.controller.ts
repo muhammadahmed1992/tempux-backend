8a7ccb560f3a1a70e0a4d523bb7cefad4cbac2c5
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-07-25 18:50:57 +0500

    fix invalid reference issue

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, HttpStatus } from "@nestjs/common";
-import { AppService } from "./app.service";
-import ResponseHelper from "@Helper/response-helper";
-import ApiResponse from "@Common/helper/api-response";
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import ResponseHelper from '@Helper/response-helper';
+import ApiResponse from '@Common/helper/api-response';
 
 @Controller()
 export class AppController {
-  @Get("/health")
-  async getHealth(): Promise<ApiResponse<boolean>> {
+  @Get('/health')
+  getHealth(): ApiResponse<boolean> {
     return ResponseHelper.CreateResponse<boolean>(
-      "API Gateway is up and running",
+      'API Gateway is up and running',
       true,
-      HttpStatus.OK
+      HttpStatus.OK,
     );
   }
 }
