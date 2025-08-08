c2fbbce084b4fcc57eee9a3d8434be8d2ba2d603
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-08-08 18:45:50 +0500

    add logging

--- auth-service/src/controllers/user.controller.ts
+++ auth-service/src/controllers/user.controller.ts
@@ -380,12 +380,12 @@ export class UserController {
     @Req() req: Request,
     @Body('email') email: string,
   ) {
-    const session = req.session as any;
-    if (session && session.user) {
+    const session = (req.session as any).user;
+    if (session) {
       return this.userService.validateExistingAccount(
         email,
-        session.socialEmail,
-        session.provider,
+        session?.socialEmail,
+        session?.provider,
       );
     }
     throw new UnauthorizedException(
@@ -395,8 +395,9 @@ export class UserController {
 
   @Post('/social-media')
   async createUserBySocialMedia(@Req() req: Request) {
-    const session = req.session as any;
-    if (session && session.user) {
+    const session = (req.session as any)?.user;
+    console.log(session);
+    if (session) {
       return this.userService.createUserBySocialLoginEmail(
         session.socialEmail,
         session.provider,
