80fa3fd913d20ed29762e2916068a0d62cd2d5b6
Muhammad Ahmed <muhammed_hm@hotmail.com>
2025-08-19 18:43:52 +0500

    Add logging

--- product-service/src/tag/tag.job.ts
+++ product-service/src/tag/tag.job.ts
@@ -8,11 +8,15 @@ export class TagCleanupJob {
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleCron() {
+    console.log('Started new arrival tagging');
     await this.tagService.cleanupExpiredNewArrivalTags();
+    console.log('Completed new arrival tagging');
   }
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handlePopularProductsCron() {
+    console.log('Started Make Popular Tagging');
     await this.tagService.markPopularProductsJob();
+    console.log('Completed Make Popular Tagging');
   }
 }
